6cf6ebe5963c5e17ceba91bd406e3c924c860998
Shields Eom <ty.eom@bucketplace.net>
2023-12-01 19:26:11 +0900

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.sumOf { s -> "${s.first { it.isDigit() }}${s.last { it.isDigit() }}".toInt() }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+        val letters = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val digitsByLetters = letters.zip(digits).toMap()
+        return input.sumOf { s ->
+            val firstDigit = s.findAnyOf(digits + letters)!!.second.let { digitsByLetters[it] ?: it }
+            val lastDigit = s.findLastAnyOf(digits + letters)!!.second.let { digitsByLetters[it] ?: it }
+            (firstDigit + lastDigit).toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_1_test")) == 142)
+    check(part2(readInput("Day01_2_test")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
