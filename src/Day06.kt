9feb46dd277c4ed9493a6b1d162a33ed0e5093e9
Shields Eom <tyv001008@gmail.com>
2023-12-07 01:28:49 +0900

    day 06

---
+++ src/Day06.kt
@@ -0,0 +1,34 @@
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main() {
+    fun solveQuadraticEquation(a: Double, b: Double, c: Double): Pair<Double, Double> =
+        (-b - sqrt(b.pow(2) - 4 * a * c)) / (2 * a) to (-b + sqrt(b.pow(2) - 4 * a * c)) / (2 * a)
+
+    fun part1(input: List<String>): Int {
+        val times = input[0].getInts()
+        val distances = input[1].getInts()
+
+        return times.zip(distances).productOf { (time, distance) ->
+            val (min, max) = solveQuadraticEquation(1.0, -time.toDouble(), distance.toDouble())
+            ceil(max - 1).toInt() - floor(min + 1).toInt() + 1
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val time = input[0].getInts().joinToString("").toLong()
+        val distance = input[1].getInts().joinToString("").toLong()
+
+        val (min, max) = solveQuadraticEquation(1.0, -time.toDouble(), distance.toDouble())
+        return ceil(max - 1).toLong() - floor(min + 1).toLong() + 1
+    }
+
+    check(part1(readInput("Day06_test")) == 288)
+    check(part2(readInput("Day06_test")) == 71503L)
+
+    val input = readInput("Day06")
+    part1(input).println()
+    part2(input).println()
+}
